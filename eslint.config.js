f3fa24742b71570b92186889c1ad82b94e0280c1
yeonilil <gwagseoyeon@gmail.com>
2024-11-13 21:17:53 +0900

    chore: eslint-plugin-tailwindcss 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,50 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import tailwind from "eslint-plugin-tailwindcss";
+import tsParser from "@typescript-eslint/parser";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tailwind.configs["flat/recommended"],
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
-  },
-)
+    settings: {
+      tailwindcss: {
+        config: "tailwind.config.js",
+        cssFiles: [
+          //Tailwind CSS 클래스를 검사할 CSS 파일의 경로 패턴
+          "**/*.css",
+          "!**/node_modules",
+          "!**/.*",
+          "!**/dist",
+          "!**/build",
+        ],
+        removeDuplicates: true, //동일한 Tailwind CSS 클래스가 중복으로 선언된 경우 이를 자동으로 제거
+        skipClassAttribute: false, //class 속성을 검사에서 제외할지 여부를 설정, 현재 모든 class 속성이 검사 대상
+      },
+    },
+  }
+);
