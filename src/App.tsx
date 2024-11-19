1e72e9b854f9238e14ba71da53441c96d88afaf5
yeonilil <gwagseoyeon@gmail.com>
2024-11-19 11:02:53 +0900

    fix: popup.html 위치 수정, 업로드 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -34,8 +33,6 @@ function App() {
       </p>
     </>
   );
-  );
 }
 
-export default App;
 export default App;
