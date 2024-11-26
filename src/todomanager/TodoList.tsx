4ebcbd5d1fd019a813d6be7ef3ea78a265095a76
guddnboy <hung3018@naver.com>
2024-11-26 23:09:20 +0900

    refactor: TodoList 컴포넌트 분리

---
+++ src/todomanager/TodoList.tsx
@@ -0,0 +1,67 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+
+interface Todo {
+  id: string;
+  content: string;
+  isComplete: boolean;
+}
+
+function TodoList() {
+  // TODO : 데이터를 indexedDB에 저장하고 관리할 수 있도록 작업 예정
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (typeof newTodo === 'string' && newTodo.length > 0) {
+      setTodos([
+        ...todos,
+        { id: uuidv4(), content: newTodo, isComplete: false },
+      ]);
+      setNewTodo('');
+    }
+  };
+
+  const completeTodo = (id: string) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, isComplete: !todo.isComplete };
+        }
+        return todo;
+      }),
+    );
+  };
+  return (
+    <div>
+      <div className="flex-col w-32 h-32">
+        <div id="todo-list" className=" h-16 overflow-y-scroll">
+          {todos.map(todo => (
+            <div key={todo.id} className="flex">
+              <div>{todo.content}</div>
+              <input
+                type="checkbox"
+                checked={todo.isComplete}
+                onChange={() => completeTodo(todo.id)}
+              />
+            </div>
+          ))}
+        </div>
+        <div className="flex">
+          <input
+            id="todo-input"
+            value={newTodo}
+            onChange={e => setNewTodo(e.target.value)}
+            type="text"
+            placeholder="할 일을 입력하세요."
+          />
+        </div>
+        <button id="add-todo" type="submit" onClick={() => addTodo()}>
+          추가
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;
