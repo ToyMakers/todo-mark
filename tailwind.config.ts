f2db13492f7c5445dcec44778f3568e6d8e1c752
guddnboy <hung3018@naver.com>
2024-11-27 15:11:46 +0900

    chore: tailwind 커스텀 클래스 추가 및 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,27 +3,7 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      components: {
-        '.custom-checkbox': {
-          appearance: 'none',
-          width: '1rem',
-          height: '1rem',
-          'background-color': '#f3f4f6',
-          'border-color': '#d1d5db',
-          'border-radius': '0.25rem',
-          'focus:ring': '2',
-          'focus:ring-color': '#f59e0b',
-          'checked:bg': '#f59e0b',
-          'checked:border-color': '#f59e0b',
-          'checked:before:content': "'✔'",
-          'checked:before:color': '#ffffff',
-          'checked:before:flex': 'true',
-          'checked:before:justify-center': 'true',
-          'checked:before:items-center': 'true',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
