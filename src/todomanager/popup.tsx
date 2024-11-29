af7dc7b7b0fb5df432616bca08fb57bf5e196a5d
yeonilil <gwagseoyeon@gmail.com>
2024-11-29 14:42:27 +0900

    stye: body font size 변경

--- src/todomanager/popup.tsx
+++ src/todomanager/popup.tsx
@@ -98,7 +98,7 @@ function Popup() {
   }, [todoFromDB]);
 
   return (
-    <div className="text-sm p-2">
+    <div className="p-2">
       <div className="w-80 h-10">
         <h1 className="text-sm">투두막</h1>
       </div>
