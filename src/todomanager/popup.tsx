f21e59b13da3e1669fb822a5b9509577abb8a8d1
guddnboy <hung3018@naver.com>
2024-12-03 22:21:28 +0900

    feat: TodoList 와 Detail props 추가

--- src/todomanager/popup.tsx
+++ src/todomanager/popup.tsx
@@ -1,23 +1,21 @@
-// import { useState } from 'react';
-// import TodoList from './todo';
+import { useState } from 'react';
 import Detail from './detail';
 
-// interface Todo {
-//   id: string;
-//   content: string;
-//   isComplete: boolean;
-// }
-
 function Popup() {
-  // const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const [selectedId, setSelectedId] = useState('');
+
+  const handleSelectTodo = (id: string) => {
+    setSelectedId(id);
+  };
 
   return (
     <div className="p-2">
       <div className="w-80 h-10">
         <h1 className="text-sm">투두막</h1>
       </div>
-      <Detail />
-      {/* {selectedTodo ? <Detail /> : <TodoList />} */}
+      <TodoList onSelectTodo={handleSelectTodo} />
+
+      <Detail id={selectedId} />
     </div>
   );
 }
