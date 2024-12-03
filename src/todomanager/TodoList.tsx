ce0ef989cfff4ca5672ddae4f660935ad1aee2c7
guddnboy <hung3018@naver.com>
2024-12-03 20:02:22 +0900

    feat: 분리된 TodoList 컴포넌트와 DB 연결

--- src/todomanager/TodoList.tsx
+++ src/todomanager/TodoList.tsx
@@ -1,64 +1,172 @@
+import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { useState } from 'react';
+import { addTodo, getAllTodos } from '../db/dbManager';
 
 interface Todo {
   id: string;
-  content: string;
+  title: string;
+  dueDate?: Date;
   isComplete: boolean;
+  todoDetail: TodoDetail;
 }
 
+interface TodoDetail {
+  description: string;
+}
 function TodoList() {
-  // TODO : 데이터를 indexedDB에 저장하고 관리할 수 있도록 작업 예정
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodo, setNewTodo] = useState('');
+  const [editTodo, setEditTodo] = useState<{
+    id: string;
+    title: string;
+  } | null>(null);
 
-  const addTodo = () => {
+  const [todoFromDB, setTodoFromDB] = useState<Todo[]>([]);
+
+  const handleNewTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTodoContent = e.target.value;
+    setNewTodo(newTodoContent);
+  };
+
+  const getTodosFromDB = async () => {
+    const todoList = await getAllTodos();
+    setTodoFromDB(
+      todoList.map(todo => ({
+        id: todo.id,
+        title: todo.title || '',
+        dueDate: todo.dueDate ? new Date(todo.dueDate) : undefined,
+        isComplete: todo.isComplete,
+        todoDetail: todo.todoDetail,
+      })),
+    );
+  };
+
+  const fetchTodos = async () => {
+    await getTodosFromDB();
+  };
+
+  const handleAddTodo = () => {
     if (typeof newTodo === 'string' && newTodo.length > 0) {
-      setTodos([
-        ...todos,
-        { id: uuidv4(), content: newTodo, isComplete: false },
-      ]);
+      const newTodoItem = {
+        id: uuidv4(),
+        title: newTodo,
+        content: newTodo,
+        dueDate: undefined,
+        isComplete: false,
+        todoDetail: { description: '' },
+      };
+      setTodos(prevTodos => [...prevTodos, newTodoItem]);
+      addTodo(newTodoItem);
       setNewTodo('');
     }
   };
 
-  const completeTodo = (id: string) => {
+  const handleCompleteTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const id = e.target.value;
     setTodos(
-      todos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, isComplete: !todo.isComplete };
-        }
-        return todo;
-      }),
+      todos.map(todo =>
+        todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo,
+      ),
     );
   };
+
+  const handleDeleteTodo = (id: string) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const handleEditTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (editTodo) {
+      setEditTodo({ ...editTodo, title: e.target.value });
+    }
+  };
+  const handleStartEditTodo = (todo: Todo) => {
+    setEditTodo({ id: todo.id, title: todo.title });
+  };
+
+  const handleSaveEditTodo = () => {
+    if (editTodo) {
+      setTodos(
+        todos.map(todo =>
+          todo.id === editTodo.id ? { ...todo, content: editTodo.title } : todo,
+        ),
+      );
+      setEditTodo(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, [todoFromDB]);
+
   return (
-    <div>
-      <div className="flex-col w-32 h-32">
-        <div id="todo-list" className=" h-16 overflow-y-scroll">
-          {todos.map(todo => (
-            <div key={todo.id} className="flex">
-              <div>{todo.content}</div>
+    <div className="p-2">
+      <div className="w-80 h-10">
+        <h1 className="text-sm">투두막</h1>
+      </div>
+      <div className="flex-col w-full ">
+        <div className="max-w-80 h-52 overflow-y-auto">
+          {todoFromDB.map(todo => (
+            <div key={todo.id} className="flex justify-between my-1">
               <input
                 type="checkbox"
                 checked={todo.isComplete}
-                onChange={() => completeTodo(todo.id)}
+                value={todo.id}
+                onChange={handleCompleteTodo}
               />
+              {editTodo?.id === todo.id ? (
+                <input
+                  type="text"
+                  value={editTodo.title}
+                  onChange={handleEditTodo}
+                  className="border rounded px-1"
+                />
+              ) : (
+                <div className="w-40 text-left break-words whitespace-normal">
+                  {todo.title}
+                </div>
+              )}
+
+              <div className="flex items-center gap-2">
+                {editTodo?.id === todo.id ? (
+                  <button
+                    type="button"
+                    onClick={handleSaveEditTodo}
+                    className="text-green-500"
+                  >
+                    저장
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={() => handleStartEditTodo(todo)}
+                    className="text-blue-500"
+                  >
+                    수정
+                  </button>
+                )}
+                <button
+                  type="button"
+                  onClick={() => handleDeleteTodo(todo.id)}
+                  className="text-red-500"
+                >
+                  삭제
+                </button>
+              </div>
             </div>
           ))}
         </div>
-        <div className="flex">
+        <div className="w-80 flex justify-between">
           <input
-            id="todo-input"
             value={newTodo}
-            onChange={e => setNewTodo(e.target.value)}
+            onChange={handleNewTodo}
             type="text"
             placeholder="할 일을 입력하세요."
+            className="w-64"
           />
+          <button type="submit" onClick={handleAddTodo}>
+            추가
+          </button>
         </div>
-        <button id="add-todo" type="submit" onClick={() => addTodo()}>
-          추가
-        </button>
       </div>
     </div>
   );
