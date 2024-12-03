23f369f2d1b755fe00d0e900502311d24e83efa1
guddnboy <hung3018@naver.com>
2024-12-03 23:03:23 +0900

    feat: detail 페이지 이동

--- src/todomanager/popup.tsx
+++ src/todomanager/popup.tsx
@@ -3,9 +3,11 @@ import Detail from './detail';
 
 function Popup() {
   const [selectedId, setSelectedId] = useState('');
+  const [currentView, setCurrentView] = useState('list');
 
-  const handleSelectTodo = (id: string) => {
+  const handleSelectTodo = (id: string, view: string) => {
     setSelectedId(id);
+    setCurrentView(view);
   };
 
   return (
@@ -13,9 +15,8 @@ function Popup() {
       <div className="w-80 h-10">
         <h1 className="text-sm">투두막</h1>
       </div>
-      <TodoList onSelectTodo={handleSelectTodo} />
-
-      <Detail id={selectedId} />
+      {currentView === 'list' && <TodoList onSelectTodo={handleSelectTodo} />}
+      {currentView === 'detail' && <Detail id={selectedId} />}
     </div>
   );
 }
