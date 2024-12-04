1ce55276ece60343dfb8c38702bdf450aecbbb42
guddnboy <hung3018@naver.com>
2024-12-04 16:13:45 +0900

    chore: 상수 분리

--- src/db/dbManager.ts
+++ src/db/dbManager.ts
@@ -1,21 +1,19 @@
-import { Todo } from '../todoSchemas';
-
-const DB_NAME = 'TODO_DB';
-const STORE_NAME = 'TODO';
+import { Todo } from './todoSchemas';
+import DB from '../constants/constants';
 
 export const createDB = () => {
   if (!window.indexedDB) {
     throw new Error('해당 브라우저에서는 indexedDB를 지원하지 않습니다.');
   }
 
-  const request = window.indexedDB.open(DB_NAME, 1);
+  const request = window.indexedDB.open(DB.NAME, 1);
 
   request.onupgradeneeded = e => {
     const target = e.target as IDBOpenDBRequest;
     const db = target.result;
 
-    if (!db.objectStoreNames.contains(STORE_NAME)) {
-      const todoStore = db.createObjectStore(STORE_NAME, { keyPath: 'id' });
+    if (!db.objectStoreNames.contains(DB.STORE_NAME)) {
+      const todoStore = db.createObjectStore(DB.STORE_NAME, { keyPath: 'id' });
 
       todoStore.createIndex('title', 'title', { unique: false });
       todoStore.createIndex('description', 'todoDetail.description', {
@@ -23,8 +21,8 @@ export const createDB = () => {
       });
     } else {
       const todoStore = db
-        .transaction(STORE_NAME, 'readwrite')
-        .objectStore(STORE_NAME);
+        .transaction(DB.STORE_NAME, 'readwrite')
+        .objectStore(DB.STORE_NAME);
 
       if (!todoStore.indexNames.contains('title')) {
         todoStore.createIndex('title', 'title', { unique: false });
@@ -48,7 +46,7 @@ export const createDB = () => {
 };
 
 export const addTodo = (todo: Todo) => {
-  const request = window.indexedDB.open(DB_NAME);
+  const request = window.indexedDB.open(DB.NAME);
 
   request.onerror = e => {
     const target = e.target as IDBOpenDBRequest;
@@ -57,20 +55,20 @@ export const addTodo = (todo: Todo) => {
 
   request.onsuccess = () => {
     const db = request.result;
-    const transaction = db.transaction([STORE_NAME], 'readwrite');
-    const store = transaction.objectStore(STORE_NAME);
+    const transaction = db.transaction([DB.STORE_NAME], 'readwrite');
+    const store = transaction.objectStore(DB.STORE_NAME);
     store.add(todo);
   };
 };
 
 export const getAllTodos = (): Promise<Todo[]> => {
   return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open(DB_NAME);
+    const request = window.indexedDB.open(DB.NAME);
 
     request.onsuccess = e => {
       const db = (e.target as IDBOpenDBRequest).result;
-      const transaction = db.transaction([STORE_NAME], 'readonly');
-      const store = transaction.objectStore(STORE_NAME);
+      const transaction = db.transaction([DB.STORE_NAME], 'readonly');
+      const store = transaction.objectStore(DB.STORE_NAME);
       const getAllRequest = store.getAll();
 
       getAllRequest.onsuccess = () => {
